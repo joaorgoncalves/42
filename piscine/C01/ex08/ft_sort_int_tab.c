37a7d3f1943ac54c7bde0409e375118d0a4b95aa
João Gonçalves <14140228+joaorgoncalves@users.noreply.github.com>
2023-03-07 20:57:58 +0000

    C01 exercises

---
+++ piscine/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrosa-go <joaorgoncalvesp@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 13:26:50 by jrosa-go          #+#    #+#             */
+/*   Updated: 2023/03/07 20:56:06 by jrosa-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int		temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				ft_swap(&tab[j], &tab[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
