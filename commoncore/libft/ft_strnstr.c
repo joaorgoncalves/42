f86e4de94b654b4b408db46bf19ca6ec6242913f
Joao Rosa Goncalves <jrosa-go@c1r9s4.42porto.com>
2023-10-23 16:02:20 +0100

    new function

--- commoncore/libft/ft_strnstr.c
+++ commoncore/libft/ft_strnstr.c
@@ -1,29 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrosa-go <jrosa-go@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: jrosa-go <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/22 21:31:59 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/10/23 15:25:29 by jrosa-go         ###   ########.fr       */
+/*   Created: 2023/10/23 15:31:38 by jrosa-go          #+#    #+#             */
+/*   Updated: 2023/10/23 15:56:19 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	comp;
 
 	i = 0;
-	while (i < n)
+	comp = 0;
+	if (!little)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+		return (big);
+	}
+	while (big || i < len)
+	{
+		comp = 0;
+		while (little[comp] == big[i + comp])
 		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			if (little[comp + 1] == '\0')
+			{
+				return (&big[i + comp]);
+			}
+			comp++;
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
