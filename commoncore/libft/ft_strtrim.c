aa9c837268a7a5eed5ea7ceeb8ae85c379b341ed
João Gonçalves <14140228+joaorgoncalves@users.noreply.github.com>
2023-10-31 15:24:03 +0000

    strtrim

--- commoncore/libft/ft_strtrim.c
+++ commoncore/libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrosa-go <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jrosa-go <jrosa-go@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/27 15:58:55 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/10/27 16:32:40 by jrosa-go         ###   ########.fr       */
+/*   Created: 2023/10/24 18:13:35 by jrosa-go          #+#    #+#             */
+/*   Updated: 2023/10/31 15:17:51 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		start;
-	int		end;
 	int		i;
-	char	*trimmedstr;
+	size_t	strsize;
+	char	*endstr;
 
 	i = 0;
-	start = 0;
-	end = ft_strlen(s1) - 1;
-	while (ft_strchr(set, s1[start++]))
+	strsize = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[i]))
 	{
+		i++;
 	}
-	while (ft_strchr(set, s1[end--]))
+	while (ft_strchr(set, s1[strsize]))
 	{
+		strsize--;
 	}
-	trimmedstr = (char *)malloc(sizeof(char) * (end - start));
-	if (!trimmedstr)
-	{
-		return (NULL);
-	}
-	while (end <= start)
-	{
-		trimmedstr[i++] = s1[start++];
-	}
-	trimmedstr[i] = '\0';
-	return (trimmedstr);
+	endstr = (char *)malloc(sizeof(char) * (strsize - i));
+	endstr = ft_substr(s1, i, (strsize - i));
+	return (endstr);
 }
