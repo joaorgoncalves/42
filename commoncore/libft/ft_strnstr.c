68bdc131ee310c1824218a55cd225c8ad4cafd9d
Joao Rosa Goncalves <jrosa-go@c1r8s11.42porto.com>
2023-11-30 17:46:04 +0000

    final strnstr correction

--- commoncore/libft/ft_strnstr.c
+++ commoncore/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jrosa-go <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:31:38 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/11/03 00:03:21 by jrosa-go         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:45:28 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	i = 0;
 	comp = 0;
-	if (!little[i] || big == NULL)
+	if (little[0] == '\0')
 	{
 		return ((char *)big);
 	}
-	while (big && i < len)
+	while (big[i] && i < len)
 	{
 		comp = 0;
-		while (little[comp] == big[i + comp] && i + comp < len)
+		while (big[i + comp] && big[i + comp] == little[comp] && i + comp < len)
 		{
 			if (little[comp + 1] == '\0')
 			{
