54f9be26c0e1614b4515095a0458cd5985787f66
Joao Rosa Goncalves <jrosa-go@c1r8s11.42porto.com>
2023-11-30 18:37:45 +0000

    substr correction

--- commoncore/libft/ft_substr.c
+++ commoncore/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jrosa-go <jrosa-go@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:46:55 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/11/11 17:24:34 by jrosa-go         ###   ########.fr       */
+/*   Updated: 2023/11/30 18:37:24 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 
 	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+	{
+		substr = malloc(1);
+		substr[0] = '\0';
+		return (substr);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	substr = (char *)malloc(sizeof(char) * (len + 1));
-	if (!substr || !s)
+	if (!substr)
 	{
 		return (NULL);
 	}
-	if (ft_strlen(s) < start)
-	{
-		return (substr);
-	}
-	while (s[start] && i < len)
+	while (i < len)
 	{
 		substr[i] = s[start + i];
 		i++;
