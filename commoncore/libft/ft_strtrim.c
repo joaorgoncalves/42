40562e129b7c4d976c6fac3becf4c66443b86595
Joao Rosa Goncalves <jrosa-go@c1r8s11.42porto.com>
2023-11-30 18:40:24 +0000

    final strtrim

--- commoncore/libft/ft_strtrim.c
+++ commoncore/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jrosa-go <jrosa-go@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:13:35 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/11/14 22:13:30 by jrosa-go         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:55:30 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*endstr;
 
 	start = 0;
-	if (!s1)
+	if (!s1 || !set)
 	{
 		return (NULL);
 	}
-	if (!set)
-	{
-		return (ft_strdup(s1));
-	}
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
 	while (s1[start] && ft_strchr(set, s1[start]))
 	{
 		start++;
 	}
-	while (s1[end] && ft_strchr(set, s1[end]))
+	while (end > start && ft_strchr(set, s1[end]))
 	{
 		end--;
 	}
