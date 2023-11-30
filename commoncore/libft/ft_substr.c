5e8d1ac9271e91e351705fe950a19c9ef8007c2e
Joao Rosa Goncalves <jrosa-go@c1r8s11.42porto.com>
2023-11-30 18:41:32 +0000

    norminette corrections

--- commoncore/libft/ft_substr.c
+++ commoncore/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jrosa-go <jrosa-go@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:46:55 by jrosa-go          #+#    #+#             */
-/*   Updated: 2023/11/30 18:37:24 by jrosa-go         ###   ########.fr       */
+/*   Updated: 2023/11/30 18:41:09 by jrosa-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = ft_strlen(s) - start;
 	substr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!substr)
-	{
 		return (NULL);
-	}
 	while (i < len)
 	{
 		substr[i] = s[start + i];
